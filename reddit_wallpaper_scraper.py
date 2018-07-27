c8e426a5d1742cd6a32117db2b0eacf262bbf6ae
li <lhuang210@gmail.com>
2018-07-26 21:05:19 -0400

    Create basic functions to store image url to local directory.

--- reddit_wallpaper_scraper.py
+++ reddit_wallpaper_scraper.py
@@ -1,8 +1,11 @@
 import util
+import config
 
 wallpaper_subreddit = util.get_subreddit('wallpaper')
-top_posts = util.get_top_posts(wallpaper_subreddit, 1)
+top_posts = util.get_top_posts(wallpaper_subreddit, 15, "all")
 image_urls = util.get_image_urls(top_posts)
 
 for url in image_urls:
     print(url)
+
+util.download_image_url(image_urls[0], 'wallpaper')
--- util.py
+++ util.py
@@ -1,22 +1,24 @@
 import config
 import praw
 import re
+import os
+import urllib.request
 
-reddit_instance = praw.Reddit(client_id = config.REDDIT_CLIENT_ID,
-                              client_secret = config.REDDIT_CLIENT_SECRET,
-                              user_agent = config.USER_AGENT)
+reddit_instance = praw.Reddit(client_id=config.REDDIT_CLIENT_ID,
+                              client_secret=config.REDDIT_CLIENT_SECRET,
+                              user_agent=config.USER_AGENT)
 
 
 def get_subreddit(subreddit_name):
     return reddit_instance.subreddit(subreddit_name)
 
 
-def get_top_posts(subreddit, num_post):
-    return subreddit.top(limit = num_post)
+def get_top_posts(subreddit, num_post, time):
+    return subreddit.top(limit=num_post, time_filter=time)
 
 
-def get_hot_posts(subreddit, num_post):
-    return subreddit.hot(limit = num_post)
+def get_hot_posts(subreddit, num_post, time):
+    return subreddit.hot(limit=num_post, time_filter=time)
 
 
 def get_image_urls(posts):
@@ -29,3 +31,17 @@ def get_image_urls(posts):
 
     return image_urls
 
+
+def download_image_url(url, dir_name):
+    ensure_dir(dir_name)
+    if not dir_name.endswith('\\'):
+        dir_name = dir_name + '\\'
+
+    urllib.request.urlretrieve(url, dir_name + url.split('/')[-1])
+
+
+def ensure_dir(dir_name):
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, dir_name)
+    if not os.path.exists(final_directory):
+        os.makedirs(final_directory)
